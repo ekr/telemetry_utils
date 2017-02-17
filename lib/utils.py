1f0e6874275b57faf42fe5d11ca9dabfb2a39ceb
EKR <ekr@rtfm.com>
2017-02-17 10:45:18 -0800

    Add a bunch of tools

--- lib/utils.py
+++ lib/utils.py
@@ -15,5 +15,45 @@ def is_not_in_tls_experiment(ping):
     except:
         return True
 
-def foo():
-    print "FOO"
+def filter_for_histogram(p, histogram):
+    if p[histogram] is None:
+        return False
+    try:
+        return p[histogram].sum() > 0
+    except:
+        return False
+
+def payload(h):
+    return "payload/histograms/%s"%h
+
+def count_reports(pings, h):
+    histogram = payload(h)
+    reduced = pings.filter(lambda p: filter_for_histogram(p, histogram))
+    accum = sc.accumulator(0)
+    reduced.foreach(lambda p: accum.add(p[histogram].sum()))
+    return accum
+
+def compare_counts(pings, a, b):
+    ca = count_reports(pings, a).value
+    cb = count_reports(pings, b).value
+    print ca, cb, ca/cb
+
+    
+def accum_histogram(accums, hist):
+    for i in range(0, len(hist)):
+        accums[i].add(hist[i])
+
+
+def sum_histogram(pings, buckets, h):
+    histogram = payload(h)
+    reduced = pings.filter(lambda p: filter_for_histogram(p, histogram))
+    accums = []
+    for i in range(0, buckets):
+        accums.append(sc.accumulator(0))
+    reduced.foreach(lambda p: accum_histogram(accums, p[histogram]))
+    res = {}
+    for i in range(0, buckets):
+        if accums[i].value != 0:
+            res[i] = accums[i].value
+    return res
+
