4e458bb068848cbc50c51deadc58ca7e8672d7ae
Sajjad Arshad <arshad@ccs.neu.edu>
2017-08-11 10:53:58 -0700

    printing logs count by the type

--- lib/tls13-middlebox-experiment.py
+++ lib/tls13-middlebox-experiment.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[4]:
+
 import ujson as json
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -63,27 +68,38 @@ def getSecurityState(d):
     except:
         return -1
 
-def process(dataset, channel, begin, end):
-    d = (dataset.where(docType='tls13-middlebox-repetition')
-                .where(appName='Firefox')
+def getLogsByType(dataset, channel, begin, end):
+    d = (dataset.where(docType="tls13-middlebox-repetition")
+                .where(appName="Firefox")
                 .where(appUpdateChannel=channel)
                 .where(submissionDate=lambda x: x >= begin and x <= end))
 
     logs = d.records(sc)
-    started = logs.filter(lambda x: x["payload"]["status"] == "started")
-    aborted = logs.filter(lambda x: x["payload"]["status"] == "aborted")
-    finished = logs.filter(lambda x: x["payload"]["status"] == "finished")
-
-    print "No. of Logs", logs.count()
-    print "No. of Started: %d", started.count()
-    print "No. of Aborted: %d", aborted.count()
-    print "No. of Finished: %d", finished.count()
-
+    started_logs = logs.filter(lambda x: x["payload"]["status"] == "started")
+    aborted_logs = logs.filter(lambda x: x["payload"]["status"] == "aborted")
+    finished_logs = logs.filter(lambda x: x["payload"]["status"] == "finished")
+    
+    return (started_logs, aborted_logs, finished_logs)
     #return [finished, summarize(finished)]
 
 
 ### STUFF TO RUN
 
 dataset = Dataset.from_source('telemetry')
-process(dataset, "nigthly", "20170701", "20170901")
+
+started_logs, aborted_logs, finished_logs = getLogsByType(dataset, "nightly", "20170701", "20170901")
+
+# print count
+started_count = started_logs.count()
+aborted_count = aborted_logs.count()
+finished_count = finished_logs.count()
+failure_count = started_count - (aborted_count + finished_count)
+total_count = started_count + aborted_count + finished_count
+
+print "Count: Total (%d), Started (%d), Aborted (%d), Finished (%d), Failure (%d)" %               (total_count, started_count, aborted_count, finished_count, failure_count)
+
+
+# In[ ]:
+
+
 
